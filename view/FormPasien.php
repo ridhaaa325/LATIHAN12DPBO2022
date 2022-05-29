bfca1ff35c35748acdb0ccea3393bb2fdb15eb79
ridhaaa325 <80692514+ridhaaa325@users.noreply.github.com>
2022-05-29 09:01:13 +0700

    Update FormPasien.php

--- view/FormPasien.php
+++ view/FormPasien.php
@@ -20,37 +20,22 @@ class FormPasien implements KontrakView
 	{	
 		$this->tpl = new Template("templates/tambah.html");
 
-		if(isset($_POST['tambah']))
+		if(isset($_POST['submit']))
 		{
 			$this->prosespasien->tambahDataPasien($_POST);
-            		header("location:index.php");
+			
+            header("location:index.php");
 		}
 
 		if(isset($_GET['id_hapus']))
 		{
 			$id = $_GET['id_hapus'];
 			$this->prosespasien->hapusDataPasien($id);
-            		header("location:index.php");
+
+            header("location:index.php");
 		}
-
-		if(isset($_POST['tambah']))
-		{
-			$this->tpl->replace("VALUE_NIK", "");
-			$this->tpl->replace("VALUE_NAMA", "");
-			$this->tpl->replace("VALUE_TEMPAT", "");
-			$this->tpl->replace("VALUE_TL", "");
-			$this->tpl->replace("VALUE_GENDER", "");
-			$this->tpl->replace("VALUE_EMAIL", "");
-			$this->tpl->replace("VALUE_TELEPON", "");
-			$this->tpl->replace("VALUE_ID", "");
-
-			$this->tpl->replace("VALUE_BUTTON", "tambah");
-		}
-
 		$this->tpl->write();
 	}
-
-
 }
 
 ?>
